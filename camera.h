567cdfd747ffb432bf9db2146ad261eed007768d
gonza <gonzalo.christobal@gmail.com>
2020-07-14 17:37:37 -0400

    defocus blur

--- camera.h
+++ camera.h
@@ -1,8 +1,3 @@
-#ifndef CAMERA_H
-#define CAMERA_H
-
-#include "utils.h"
-
 class camera
 {
 public:
@@ -10,28 +5,36 @@ public:
         point3 lookfrom,
         point3 lookat,
         vec3 vup,
-        double vfov,
-        double aspect_ratio)
+        double vfov, // vertical field-of-view in degrees
+        double aspect_ratio,
+        double aperture,
+        double focus_dist)
     {
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
         auto viewport_height = 2.0 * h;
         auto viewport_width = aspect_ratio * viewport_height;
-        auto focal_length = 1.0;
 
-        auto w = unit_vector(lookfrom - lookat);
-        auto u = unit_vector(cross(vup, w));
-        auto v = cross(w, u);
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
 
         origin = lookfrom;
-        horizontal = viewport_width * u;
-        vertical = viewport_height * v;
-        lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
+        horizontal = focus_dist * viewport_width * u;
+        vertical = focus_dist * viewport_height * v;
+        lower_left_corner = origin - horizontal / 2 - vertical / 2 - focus_dist * w;
+
+        lens_radius = aperture / 2;
     }
 
-    ray get_ray(double u, double v) const
+    ray get_ray(double s, double t) const
     {
-        return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+        vec3 rd = lens_radius * random_in_unit_disk();
+        vec3 offset = u * rd.x() + v * rd.y();
+
+        return ray(
+            origin + offset,
+            lower_left_corner + s * horizontal + t * vertical - origin - offset);
     }
 
 private:
@@ -39,5 +42,6 @@ private:
     point3 lower_left_corner;
     vec3 horizontal;
     vec3 vertical;
+    vec3 u, v, w;
+    double lens_radius;
 };
-#endif
--- main.cpp
+++ main.cpp
@@ -65,7 +65,14 @@ int main()
   world.add(make_shared<sphere>(point3(1, 0, -1), 0.5, make_shared<metal>(color(.8, .6, .2), 0.3)));
   world.add(make_shared<sphere>(point3(-1, 0, -1), 0.5, make_shared<dielectric>(1.5)));
   world.add(make_shared<sphere>(point3(-1, 0, -1), -0.45, make_shared<dielectric>(1.5)));
-  camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 20, aspect_ratio);
+
+  point3 lookfrom(3, 3, 2);
+  point3 lookat(0, 0, -1);
+  vec3 vup(0, 1, 0);
+  auto dist_to_focus = (lookfrom - lookat).length();
+  auto aperture = 2.0;
+
+  camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
   for (int j = image_height - 1; j >= 0; --j)
   {
